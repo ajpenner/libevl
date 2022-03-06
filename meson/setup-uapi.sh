f74628100996d90614fa39936ffd76e15ae26d69
Andrew Penner <andrew.penner@protonmail.com>
2022-03-06 12:12:47 +0000

    Make meson copy instead of symlinks

--- meson/setup-uapi.sh
+++ meson/setup-uapi.sh
@@ -9,11 +9,11 @@ mkdir -p $O_UAPI
 rm -f $O_UAPI/asm $O_UAPI/asm-generic $O_UAPI/evl
 
 if test -r $UAPI/Kbuild; then
-    ln -s $UAPI/arch/$ARCH/include/uapi/asm $O_UAPI/asm
-    ln -s $UAPI/include/uapi/asm-generic $O_UAPI/asm-generic
-    ln -s $UAPI/include/uapi/evl $O_UAPI/evl
+    cp -r $UAPI/arch/$ARCH/include/uapi/asm $O_UAPI/asm
+    cp -r $UAPI/include/uapi/asm-generic $O_UAPI/asm-generic
+    cp -r $UAPI/include/uapi/evl $O_UAPI/evl
 elif test \! -L	$O_UAPI/asm; then
-    ln -s $UAPI/asm $O_UAPI/asm
-    ln -s $UAPI/asm-generic $O_UAPI/asm-generic
-    ln -s $UAPI/evl $O_UAPI/evl
+    cp -r $UAPI/asm $O_UAPI/asm
+    cp -r $UAPI/asm-generic $O_UAPI/asm-generic
+    cp -r $UAPI/evl $O_UAPI/evl
 fi
