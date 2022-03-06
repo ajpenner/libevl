42f4291259e2fb55f6d0627e99697714ed803814
Andrew Penner <andrew.penner@protonmail.com>
2022-03-06 11:22:53 +0000

    Retore original setup-uapi.sh

--- meson/setup-uapi.sh
+++ meson/setup-uapi.sh
@@ -1,9 +1,6 @@
 #! /bin/sh
 
-echo $UAPI
-realUAPI=`realpath $UAPI`
-
-if test \! -d $realUAPI/include/uapi/evl; then
+if test \! -d $UAPI/include/uapi/evl; then
     echo "meson: path given to -Duapi does not look right ($UAPI/include/uapi/evl is missing)"
     exit 1
 fi
@@ -11,12 +8,12 @@ fi
 mkdir -p $O_UAPI
 rm -f $O_UAPI/asm $O_UAPI/asm-generic $O_UAPI/evl
 
-if test -r $realUAPI/Kbuild; then
-    ln -s $realUAPI/arch/$ARCH/include/uapi/asm $O_UAPI/asm
-    ln -s $realUAPI/include/uapi/asm-generic $O_UAPI/asm-generic
-    ln -s $realUAPI/include/uapi/evl $O_UAPI/evl
+if test -r $UAPI/Kbuild; then
+    ln -s $UAPI/arch/$ARCH/include/uapi/asm $O_UAPI/asm
+    ln -s $UAPI/include/uapi/asm-generic $O_UAPI/asm-generic
+    ln -s $UAPI/include/uapi/evl $O_UAPI/evl
 elif test \! -L	$O_UAPI/asm; then
-    ln -s $realUAPI/asm $O_UAPI/asm
-    ln -s $realUAPI/asm-generic $O_UAPI/asm-generic
-    ln -s $realUAPI/evl $O_UAPI/evl
+    ln -s $UAPI/asm $O_UAPI/asm
+    ln -s $UAPI/asm-generic $O_UAPI/asm-generic
+    ln -s $UAPI/evl $O_UAPI/evl
 fi
